1a206af7901888fe833bbebfba989e58a85f91ae
arthurpnl <arthurpnlcontact1@gmail.com>
2025-03-10 14:42:12 +0100

    Add bzero/calloc

--- src/utils.c
+++ src/utils.c
@@ -6,11 +6,37 @@
 /*   By: arpenel <arpenel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/07 15:24:19 by arpenel           #+#    #+#             */
-/*   Updated: 2025/03/07 15:24:37 by arpenel          ###   ########.fr       */
+/*   Updated: 2025/03/10 14:35:50 by arpenel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "push_swap.h"
+#include <stdio.h>
+#include <unistd.h>
+#include <limits.h>
+#include <stdlib.h>
+
+void	ft_bzero(void *s, size_t n)
+{
+	char	*str;
+
+	str = s;
+	while (n)
+	{
+		*str++ = 0;
+		n--;
+	}
+}
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	void	*ptr;
+
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+		return (ptr);
+	ft_bzero(ptr, size * nmemb);
+	return (ptr);
+}
 
 static int	ft_len_words(const char *str, char sep)
 {
