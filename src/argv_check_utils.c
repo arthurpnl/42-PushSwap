61693e986d903e633fc29daeb8b84487c8957bb5
arthurpnl <arthurpnlcontact1@gmail.com>
2025-03-10 14:06:35 +0100

    fix space functions/valid number

--- src/argv_check_utils.c
+++ src/argv_check_utils.c
@@ -6,63 +6,70 @@
 /*   By: arpenel <arpenel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/07 14:45:44 by arpenel           #+#    #+#             */
-/*   Updated: 2025/03/08 12:36:11 by arpenel          ###   ########.fr       */
+/*   Updated: 2025/03/10 14:06:06 by arpenel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "push_swap.h"
+#include <stdio.h>
+#include <unistd.h>
+#include <limits.h>
 
 int is_valid_number(char *str)
 {
     int i;
-    int is_it_valid;
-
+    
     i = 0;
-    is_it_valid = 0;
-    if(str[0] == '\0')
+    if(!str[i])
+        return (0);
+    if (str[i] == '+' || str[i] == '-')
+        i++;
+    if (!str[i] || (str[i] < '0' && str[i] > '9'))
         return (0);
     while (str[i])
     {
-        if (str[0] == '+' && str[i + 1] >= '0' && str[i + 1] <= '9')
-            is_it_valid = 1;
-        else
+        if (str[i] < '0' || str[i] > '9')
             return (0);
-        if (str[0] == '-' && str[i + 1] >= '0' && str[i + 1] <= '9')
-            is_it_valid = 1;
-        else
-            return (0);
-        if (str[i] >= '0' && str[i] <= '9')
-            is_it_valid = 1;
+        i++;
     }
-    return (is_it_valid);
-}
-
-char **parse_argument(int   argc, char **argv)
-{
-    ft_split(str, ' ');
-}
-
-char    *parse_single_argument(char *str)
-{
-    
+    return (1);
 }
 
 int is_there_space(char *str)
 {
     int i;
-    int is_there_space;
     
     i = 0;
-    is_a_string = 0;
+    if (!str[i])
+        return (0);
     while (str[i])
     {
         if(str[i] == ' ')
-        {
-            is_a_string = 1;
-            return (is_there_space);
-        }
-        else
-            i++;
+            return (1);
+        i++;
     }
-    return (is_there_space);
+    return (0);
 }
+
+t_node *parse_argument(int  argc, char **argv)
+{
+    if (is_there_space(argv[1]) == 1 )
+    ft_split(str, ' ');
+}
+    
+/*
+int main(void)
+{
+    int i;
+
+    i = 0;
+    char *test_case[] = {
+        "42", "43 234 535  345", "32234", "", NULL
+    };
+    while (test_case[i])
+    {
+        printf("Test \"%s\" → %d\n", test_case[i], is_there_space(test_case[i]));
+        i++;
+    }
+    return (0);
+}
+*/
