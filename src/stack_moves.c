97c80d3e88db425fe1e8d69e3a4064a8d922f480
arthurpnl <arthurpnlcontact1@gmail.com>
2025-03-15 15:30:39 +0100

    Add stack moves

--- src/stack_moves.c
+++ src/stack_moves.c
@@ -6,7 +6,7 @@
 /*   By: arpenel <arpenel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/14 15:35:28 by arpenel           #+#    #+#             */
-/*   Updated: 2025/03/14 16:16:16 by arpenel          ###   ########.fr       */
+/*   Updated: 2025/03/15 15:18:08 by arpenel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,25 @@ void    sa(t_list   **stack_a)
     t_list *first;
     t_list *second;
     
-    if(!(stack_a) || !(*stack_a)->next)
-        return ;
+    if (!(*stack_a) || !(*stack_a)->next)
+        return ; 
     first = *stack_a;
-    second - first->next;
-
+    second = (*stack_a)->next;
     first->next = second->next;
+    second->next = first;
+    *stack_a = second;
+}
+
+void    sb(t_list **stack_b)
+{
+    t_list  *first;
+    t_list  *second;
+
+    if (!(*stack_b) || !(*stack_b)->next)
+        return ;
+    first = *stack_b;
+    second = (*stack_b)->next;
+    first->next = second->next;
+    second->next = first;
+    *stack_b = second;
 }
