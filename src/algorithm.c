50cf91bef6db3eff64aa5a239eb2629ebbf19860
arthurpnl <arthurpnlcontact1@gmail.com>
2025-03-20 16:16:51 +0100

    add simple_sort_3

--- src/algorithm.c
+++ src/algorithm.c
@@ -25,6 +25,36 @@ void    send_first_two(t_list **stack_a, t_list **stack_b)
     else if (total_elements = 4)
         pb(stack_a, stack_b);
     else if (total_elements <= 3)
-        simple_sort(stack_a);
+        simple_sort_3(stack_a);
+}
+
+void    simple_sort_3(t_list **stack_a)
+{
+    int    first;
+    int    second;
+    int    third;
+
+    first = (*stack_a)->value;
+    second = (*stack_a)->next->value;
+    third = (*stack_a)->next->next->value;
+
+    if (first > second && second > third && first > third)
+    {
+        sa(stack_a);
+        rra(stack_a);
+    }
+    else if (first > second && second < third && first > third)
+        ra(stack_a);
+    else if (first < second && second > third && first > third)
+        rra(stack_a);
+    else if (first > second && second < third && first > third)
+        sa(stack_a);
+    else if (first < second && second > third && first < third)
+    {
+        sa(stack_a);
+        ra(stack_a);
+    }
+    else
+        return (0);
 }
 
