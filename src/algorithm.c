73738f21af467c3a8c633b1eb057bfb5239ddc09
arthurpnl <arthurpnlcontact1@gmail.com>
2025-03-20 14:32:46 +0100

    fix on send_first_two

--- src/algorithm.c
+++ src/algorithm.c
@@ -14,7 +14,17 @@
 
 void    send_first_two(t_list **stack_a, t_list **stack_b)
 {
-    pb(stack_a, stack_b);
-    pb(stack_a, stack_b);
+    int    total_elements;
+
+    total_elements = count_stack_a(stack_a);
+    if (total_elements >= 5)
+    {
+        pb(stack_a, stack_b);
+        pb(stack_a, stack_b);
+    }
+    else if (total_elements = 4)
+        pb(stack_a, stack_b);
+    else if (total_elements <= 3)
+        simple_sort(stack_a);
 }
 
