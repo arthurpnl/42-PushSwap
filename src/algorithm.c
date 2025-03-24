19d773f82095dafa93ad455427750f80682f0d80
arthurpnl <arthurpnlcontact1@gmail.com>
2025-03-24 17:08:22 +0100

    Add find_cheapest_node + organized *.c

--- src/algorithm.c
+++ src/algorithm.c
@@ -44,7 +44,7 @@ void    send_first_two(t_list **stack_a, t_list **stack_b)
 {
     int    total_elements;
 
-    total_elements = count_stack_lenght(stack_a);
+    total_elements = stack_lenght(stack_a);
     if (total_elements >= 5)
     {
         pb(stack_a, stack_b);
@@ -55,6 +55,90 @@ void    send_first_two(t_list **stack_a, t_list **stack_b)
     else if (total_elements <= 3)
         simple_sort_3(stack_a);
 }
+
+void find_target_node(t_list *a_node, t_list **stack_b)
+{
+    t_list *current;
+    t_list *best_target;
+    int    closest_smaller;
+
+    current = *stack_b;
+    best_target = NULL;
+    closest_smaller = INT_MIN;
+    while (current)
+    {
+        if (current->value < a_node->value && current->value > closest_smaller)
+        {
+            closest_smaller = current->value;
+            best_target = current;
+        }
+        current = current->next;
+    }
+    if (!best_target || a_node->value < find_min_value(stack_b) || a_node->value > find_max_value(stack_b))
+        find_max_position(a_node, stack_b);
+    else
+        a_node->target = best_target;
+}
+
+void	find_max_position(t_list *a_node, t_list **stack_b)
+{
+	t_list *current;
+	int    max_value;
+
+	current = (*stack_b);
+	max_value = find_max_value(stack_b);
+	while (current->value != max_value)
+		current = current->next;
+	a_node->target = current;
+}
+
+void    calculate_cost(t_list *stack_a, t_list *stack_b)
+{
+    t_list *current;
+    int    size_a;
+    int    size_b;
+
+    size_a = stack_lenght(stack_a);
+    size_b = stack_lenght(stack_b);
+    current = stack_a;
+    while(current)
+    {
+        if (current->median == 1)
+            current->cost = current->index;
+        else
+            current->cost = size_a - current->index;
+        if (current->target->median == 1)
+            current->cost += current->target->index;
+        else
+            current->cost += size_b - current->target->index;
+        current = current->next;
+    }
+}
+
+void	find_cheapest_node(t_list *stack_a)
+{
+	t_list *current;
+	t_list *cheapest_node;
+	int	min_cost;
+
+	cheapest_node = NULL;
+	current = stack_a;
+	min_cost = INT_MAX;
+	while (current)
+	{
+		current->cheapest = false;
+		if(current->cost < min_cost)
+		{
+			min_cost = current->cost;
+			cheapest_node = current;
+		}
+		current = current->next;
+	}
+	if (cheapest_node)
+		cheapest_node->cheapest = true;
+}
+
+
 
 
 
--- src/sort_utils.c
+++ src/sort_utils.c
@@ -21,6 +21,7 @@ void    assign_index(t_list **stack)
 
     current = *stack;
     while (current)
+
     {
         count = 0;
         compare = *stack;
@@ -134,64 +135,7 @@ int    stack_lenght(t_list *stack);
     return (count);
 }
 
-void find_target_node(t_list *a_node, t_list **stack_b)
-{
-    t_list *current;
-    t_list *best_target;
-    int    closest_smaller;
 
-    current = *stack_b;
-    best_target = NULL;
-    closest_smaller = INT_MIN;
-    while (current)
-    {
-        if (current->value < a_node->value && current->value > closest_smaller)
-        {
-            closest_smaller = current->value;
-            best_target = current;
-        }
-        current = current->next;
-    }
-    if (!best_target || a_node->value < find_min_value(stack_b) || a_node->value > find_max_value(stack_b))
-        find_max_position(a_node, stack_b);
-    else
-        a_node->target = best_target;
-}
-
-void	find_max_position(t_list *a_node, t_list **stack_b)
-{
-	t_list *current;
-	int    max_value;
-
-	current = (*stack_b);
-	max_value = find_max_value(stack_b);
-	while (current->value != max_value)
-		current = current->next;
-	a_node->target = current;
-}
-
-void    calculate_cost(t_list *stack_a, t_list *stack_b)
-{
-    t_list *current;
-    int    size_a;
-    int    size_b;
-
-    size_a = stack_lenght(stack_a);
-    size_b = stack_lenght(stack_b);
-    current = stack_a;
-    while(current)
-    {
-        if (current->median == 1)
-            current->cost = current->index;
-        else
-            current->cost = size_a - current->index;
-        if (current->target->median == 1)
-            current->cost += current->target->index;
-        else
-            current->cost += size_b - current->target->index;
-        current = current->next;
-    }
-}
 // trouver la meilleure position
 // calculer le cout de push/good positions
 // push le nombre le moins couteux au dessus de son target node
